7dd426a4d512702f8c6d4cd7cf17229e68eae8ea
Евгений Ильичев <888ilichoff@mail.ru>
2023-03-18 08:55:55 +0300

    Реализация метода изменения данных в менеджере

--- Homework10Console/Models/Personnel/Manager.cs
+++ Homework10Console/Models/Personnel/Manager.cs
@@ -10,7 +10,7 @@ namespace Homework10Console.Models.Personnel
 {
     internal class Manager : Personnel
     {
-        /// <summary>
+        // <summary>
         /// Просмотр данных
         /// </summary>
         /// <returns></returns>
@@ -21,6 +21,64 @@ namespace Homework10Console.Models.Personnel
             return baseClients;
         }
 
+        static void EditDataClient()
+        {
+            string id;
+            bool idBool = false;
+            string phoneNumber;
+            List<Client> baseCompared = LoadOrSaveFile.FileExistenceCheck();
+            //Выполняется пока не совпадет айди
+            while (!idBool)
+            {
+                Console.WriteLine(@"Введите ID клиента, данные 
+которого хотите изменить?
+Для выхода в прошлое меню нажмите Enter");
+                id = Console.ReadLine();
+                if (id == "")
+                {
+                    break;
+                }
+                Int32.TryParse(id, out int idInt);
+                foreach (var client in baseCompared)
+                {
+                    if (client.Id == idInt)
+                    {
+                        bool lengthNumber = false;
+                        //Выполняется пока длина вводимого номера не будет соотвествовать неободимому формату
+                        while (!lengthNumber)
+                        {
+                            Console.WriteLine(@"Введите номер телефона(11 цифр в формате 89181234567)
+Для выхода в прошлое меню нажмите Enter");
+                            phoneNumber = Console.ReadLine();
+                            if (phoneNumber == "")
+                            {
+                                break;
+                            }
+                            char[] phoneNumberChar = phoneNumber.ToArray();
+                            if (phoneNumberChar.Length == 11)
+                            {
+                                client.PhoneNumber = PhoneNumberClass.InputPhoneNumber(phoneNumberChar);
+                                Console.WriteLine("Номер изменен");
+                                BaseClients baseClients = new BaseClients(baseCompared);
+                                DataSaveJson.SerializeBaseJson();
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Не верное количество цифр в номере, введите заново");
+                            }
+                        }
+                        idBool = true;
+                        break;
+                    }
+                }
+                if (!idBool)
+                {
+                    Console.WriteLine("Клиент не найден");
+                }
+            }
+        }
+
         public static void Menu()
         {
             bool exit = true;
@@ -28,23 +86,14 @@ namespace Homework10Console.Models.Personnel
             {
                 Console.Write(@"Что делать ? 
 0 - Показать
-1 - Cохранить 
-2 - Загрузить 
-3 - Изменить номер телефона  
-4 - Выход 
+1 - Изменить данные  
+2 - Выход 
 ");
                 switch (Console.ReadLine())
                 {
                     case "0": Console.WriteLine(ViewBaseData().ToString()); break;
-                    case "1":
-                        {
-                            DataSaveJson.SerializeBaseJson();
-                            Console.WriteLine("Base saved");
-                            break; 
-                        }
-                    case "2": break;
-                    case "3": break;
-                    case "4": exit = false; break;
+                    case "1": EditDataClient(); break;
+                    case "2": exit = false; break;
                     default:
                         Console.WriteLine("Угу, так и сделаем");
                         break;
